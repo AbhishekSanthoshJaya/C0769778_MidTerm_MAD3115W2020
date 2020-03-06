4ff56b9d42e94ddd4f4e953d85c36d571339b5c1
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-03-06 15:51:08 -0500

    [Abhishek]Added validation methods

---
+++ C0769778_MidTerm_MAD3115W2020/Exceptions.swift
@@ -0,0 +1,34 @@
+//
+//  Exceptions.swift
+//  C0769778_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-06.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum CustomerErrors: Error
+{
+    case invalidEmail(email: String)
+    case invalidNumber(number: String)
+}
+
+struct Validations {
+private static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+static func email(email: String) throws {
+    
+    let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+    if !emailPred.evaluate(with: email){
+        throw CustomerErrors.invalidEmail(email: email)
+    }
+}
+    
+static func mobileNumber(number: String) throws {
+    if number.count != 10 {
+        throw CustomerErrors.invalidNumber(number: number)
+    }
+  }
+}
